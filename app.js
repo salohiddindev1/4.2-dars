d94644e4e797cd282f69f9a295a4bbde0a205c5a
salohiddindev1 <169024491+salohiddindev1@users.noreply.github.com>
2024-06-19 13:38:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let string = prompt("stringdagi malumot kiriting!");
+
+let number = prompt("numberdagi malumot kiriting!");
+
+let boolean = prompt("booleandagi bitta malumot kiriting!");
+
+alert("togri malumot kiritganingizga ishonchingiz komilmi");
+
+alert(
+  "string yozganingiz: " +
+    string +
+    "\n number yozganingiz: " +
+    number +
+    "\n boolean yozganingiz: " +
+    boolean
+);
